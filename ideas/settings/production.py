34229ad7838728a21c7230b352ddf7fdf03c1471
immanoj16 <mmaharana14@gmail.com>
2017-03-20 01:58:44 -0700

    New Page Handler view

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -27,16 +27,16 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['cfideas.herokuapp.com']
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com' #my gmail username
-EMAIL_HOST_PASSWORD = 'yourpassword' #my gmail password
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Justin <hungrypy@gmail.com>"
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'youremail@gmail.com' #my gmail username
+# EMAIL_HOST_PASSWORD = 'yourpassword' #my gmail password
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = "Justin <hungrypy@gmail.com>"
 
 
-ADMINS = [('Justin', EMAIL_HOST_USER)]
-MANAGERS = ADMINS
+# ADMINS = [('Justin', EMAIL_HOST_USER)]
+# MANAGERS = ADMINS
 
 # Application definition
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'ideas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
