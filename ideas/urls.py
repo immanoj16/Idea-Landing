a9ad3a8cbae35ad6b8e0c9c335d3832a58e67429
immanoj16 <mmaharana14@gmail.com>
2017-03-21 00:19:22 -0700

    Add AJAX join form

--- ideas/urls.py
+++ ideas/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 from pages.views import HomeView
+from newsletter.api.views import JoinCreateAPIView
 
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/email/join/$', JoinCreateAPIView.as_view(), name='email-join'),
 ]
