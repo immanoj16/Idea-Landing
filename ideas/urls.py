a9a3132d23b263198d33dd3237882ce79c5a0acb
immanoj16 <mmaharana14@gmail.com>
2017-03-21 20:45:22 -0700

    Update pages for slug field

--- ideas/urls.py
+++ ideas/urls.py
@@ -22,7 +22,7 @@ from newsletter.api.views import JoinCreateAPIView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^(?P<slug>[\w-]+)/$', PageDetailView.as_view(), name='page-detail'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/email/join/$', JoinCreateAPIView.as_view(), name='email-join'),
+    url(r'^(?P<slug>[\w-]+)/$', PageDetailView.as_view(), name='page-detail'),
 ]
